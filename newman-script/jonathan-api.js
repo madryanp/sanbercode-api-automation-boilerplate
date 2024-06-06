956313e55fa24468ac9ae62b42a2afb2dfd144f9
jfrelis <jonathanfrelis672@gmail.com>
2024-06-06 19:21:06 +0700

    commit-message

--- newman-script/jonathan-api.js
+++ newman-script/jonathan-api.js
@@ -1,7 +1,7 @@
 const newman = require("newman")
 
 newman.run({
-    collection: "json-collection/jojo.postman_collection.json",
+    collection: "json-collection/jojo.postman_collection.jso",
     environment: "json-env/jojo.postman_environment.json",
     reporters: ["cli", "htmlextra"]
 })
